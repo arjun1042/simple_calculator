8854fcdc84db73c404340154fdda22cb1e2a8c3b
Repana_Dhanarjana <124077684+arjun1042@users.noreply.github.com>
2025-02-08 00:41:05 +0530

    Add additional test cases for addition

--- test_calculator.py
+++ test_calculator.py
@@ -15,7 +15,7 @@ class TestCalculator(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(2, 1), 1)
         self.assertEqual(self.calc.subtract(1, 1), 0)
-        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(1, -1), 0)
 
 if __name__ == '__main__':
     unittest.main()
